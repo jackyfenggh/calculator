bcd965080b073253d2c3e7e42b62c5728524b96e
Jacky Feng <jackyfeng1992@gmail.com>
2021-05-17 02:49:34 +1000

    Add event listeners for 0, 1, 5 and add and equals function

--- app.js
+++ app.js
@@ -1,33 +1,97 @@
-function add(numberOne, numberTwo) {
-  return numberOne + numberTwo;  
+function add(a, b) {
+  return a + b;  
 }
 
-function subtract(numberOne, numberTwo) {
-  return numberOne - numberTwo; 
+function subtract(a, b) {
+  return a - b; 
 }
 
-function multiply(numberOne, numberTwo) {
-  return numberOne * numberTwo; 
+function multiply(a, b) {
+  return a * b; 
 }
 
-function divide(numberOne, numberTwo) {
-  return numberOne / numberTwo; 
+function divide(a, b) {
+  return a / b; 
 }
 
-function operate(numberOne, numberTwo, operator) {
+function operate(a, b, operator) {
   if (operator === '+') {
-    return add(numberOne, numberTwo);
+    return add(a, b);
   }
 
   if (operator === '-') {
-    return subtract(numberOne, numberTwo);
+    return subtract(a, b);
   }
 
   if (operator === '*') {
-    return multiply(numberOne, numberTwo);
+    return multiply(a, b);
   }
 
   if (operator === '/') {
-    return divide(numberOne, numberTwo);
+    return divide(a, b);
   }
 }
+
+var operationNumberOneString = '';
+var operationNumberTwoString = '';
+var operationType = null;
+var operationResult = null;
+var calculatorDisplayText = document.getElementById('calculator-display-text');
+
+function clickNumberButton(number) {
+  if (operationNumberOneString === operationNumberTwoString || Number.isInteger(operationResult)) {
+    operationResult = null;
+    operationNumberOneString = '';
+    calculatorDisplayText.innerText = '';
+  }
+  
+  operationNumberOneString += number;
+  calculatorDisplayText.innerText += `${number}`;
+}
+
+function clickOperationButton(operation) {
+  operationType = operation;
+  operationNumberTwoString = operationNumberOneString;
+}
+
+function clickEqualsButton() {
+  if (operationType === 'add') {
+    var a = parseInt(operationNumberOneString);
+    var b = parseInt(operationNumberTwoString);
+    
+    if (operationResult != null) {
+      calculatorDisplayText.innerText = add(a, operationResult);
+      operationResult = add(a, operationResult);
+    } else {
+      calculatorDisplayText.innerText = add(a, b);
+      operationResult = add(a, b);
+    }
+  }
+}
+
+function setupEventListeners() {
+  var calculatorButtons = document.getElementById('calculator-buttons');
+  calculatorButtons.addEventListener('click', function(e) {
+    if (e.target.parentElement.id === 'number-button-0') {
+      clickNumberButton('0');
+    }
+
+    if (e.target.parentElement.id === 'number-button-1') {
+      clickNumberButton('1');
+    }
+
+    if (e.target.parentElement.id === 'number-button-5') {
+      clickNumberButton('5');
+    }
+
+    if (e.target.parentElement.id === 'add-button') {
+      clickOperationButton('add');
+    }
+
+    if (e.target.parentElement.id === 'equals-button') {
+      clickEqualsButton();
+    }
+  });
+}
+
+setupEventListeners();
