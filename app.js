f1f925884da5a28e289745d635f80b1bb7ef4f72
Jacky Feng <jackyfeng1992@gmail.com>
2021-05-19 21:36:46 +1000

    Remove operate function

--- app.js
+++ app.js
@@ -14,24 +14,6 @@ function divide(a, b) {
   return a / b; 
 }
 
-// function operate(a, b, operator) {
-//   if (operator === '+') {
-//     return add(a, b);
-//   }
-
-//   if (operator === '-') {
-//     return subtract(a, b);
-//   }
-
-//   if (operator === '*') {
-//     return multiply(a, b);
-//   }
-
-//   if (operator === '/') {
-//     return divide(a, b);
-//   }
-// }
-
 var operationBaseNumberString = '';
 var operationOperatingNumberString = '';
 var operationType = null;
