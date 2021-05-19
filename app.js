c79082050fa06e3a6f01a30b4eedfd7c6bbf36bb
Jacky Feng <jackyfeng1992@gmail.com>
2021-05-19 23:45:15 +1000

    Revert "Refactor event listener setup for number buttons and fix logic error in clickOperationButton function"
    This reverts commit 78c6ed691a7e5e701df710489bf45429d693a408.

--- app.js
+++ app.js
@@ -1,33 +1,17 @@
 function add(a, b) {
-  if (b === '') {
-    return a + a;
-  } else {
-    return a + b;  
-  }
+  return a + b;  
 }
 
 function subtract(a, b) {
-  if (b === '') {
-    return a - a;
-  } else {
-    return a - b;  
-  }
+  return a - b; 
 }
 
 function multiply(a, b) {
-  if (b === '') {
-    return a * a;
-  } else {
-    return a * b;  
-  }
+  return a * b; 
 }
 
 function divide(a, b) {
-  if (b === '') {
-    return a / a;
-  } else {
-    return a / b;  
-  }
+  return a / b; 
 }
 
 var operationBaseNumberString = '';
@@ -38,6 +22,12 @@ var calculatorDisplayText = document.getElementById('calculator-display-text');
 var calculatorState = 'takingBaseNumber';
 
 function clickNumberButton(number) {
+  if (Number.isInteger(operationResult)) {
+    operationResult = null;
+    operationBaseNumberString = '';
+    calculatorDisplayText.innerText = '';
+  }
+  
   if (calculatorState === 'takingBaseNumber') {
     operationBaseNumberString += number;
     calculatorDisplayText.innerText = operationBaseNumberString;
@@ -45,23 +35,38 @@ function clickNumberButton(number) {
   
   if (calculatorState === 'takingOperatingNumber') {
     operationOperatingNumberString += number;
-    calculatorDisplayText.innerText = operationOperatingNumberString;
+    calculatorDisplayText.innerText = operationOperatingNumberString
   }
 }
 
 function clickOperationButton(operation) {
   operationType = operation;
-
-  if (Number.isInteger(operationResult)) {
-    operationBaseNumberString = operationResult;
-    operationOperatingNumberString = '';
-    calculatorState = 'takingOperatingNumber';
-  }
-
-  if (!Number.isInteger(operationResult)) {
-    calculatorState = 'takingOperatingNumber';
-  }
+  calculatorState = 'takingOperatingNumber';
 }
+
+// function clickEqualsButton() {
+//   var a = parseInt(operationBaseNumberString);
+//   var b = parseInt(operationOperatingNumberString);
+  
+//   if (operationType === 'add') {
+//     if (Number.isInteger(operationResult)) {
+//       calculatorDisplayText.innerText = add(operationResult, b);
+//       operationResult = add(operationResult, b);
+//     } else {
+//       calculatorDisplayText.innerText = add(a, b);
+//       operationResult = add(a, b);
+//     }
+//   }
+
+//   if (operationType === 'subtract') {
+//     if (Number.isInteger(operationResult)) {
+//       calculatorDisplayText.innerText = subtract(operationResult, b);
+//       operationResult = subtract(operationResult, b);
+//     } else {
+//       calculatorDisplayText.innerText = subtract(a, b);
+//       operationResult = subtract(a, b);
+//     }
+//   }
 
 function updateOperationResultAndDisplayText(operationType) {
   var a = parseInt(operationBaseNumberString);
@@ -84,67 +89,24 @@ function clickEqualsButton() {
   if (operationType === subtract) {
     updateOperationResultAndDisplayText(operationType);
   }
-
-  if (operationType === multiply) {
-    updateOperationResultAndDisplayText(operationType);
-  }
-
-  if (operationType === divide) {
-    updateOperationResultAndDisplayText(operationType);
-  }
-
+  
   calculatorState = 'takingBaseNumber';
 }
 
 function setupEventListeners() {
   var calculatorButtons = document.getElementById('calculator-buttons');
   calculatorButtons.addEventListener('click', function(e) {
-    var buttonNumbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    buttonNumbersArray.forEach(function(number) {
-      if (e.target.parentElement.id === `number-button-${number}`) {
-        clickNumberButton(`${number}`);
-      }
-    })
-    
-    // if (e.target.parentElement.id === 'number-button-0') {
-    //   clickNumberButton('0');
-    // }
+    if (e.target.parentElement.id === 'number-button-0') {
+      clickNumberButton('0');
+    }
 
-    // if (e.target.parentElement.id === 'number-button-1') {
-    //   clickNumberButton('1');
-    // }
+    if (e.target.parentElement.id === 'number-button-1') {
+      clickNumberButton('1');
+    }
 
-    // if (e.target.parentElement.id === 'number-button-2') {
-    //   clickNumberButton('2');
-    // }
-
-    // if (e.target.parentElement.id === 'number-button-3') {
-    //   clickNumberButton('3');
-    // }
-
-    // if (e.target.parentElement.id === 'number-button-4') {
-    //   clickNumberButton('4');
-    // }
-
-    // if (e.target.parentElement.id === 'number-button-5') {
-    //   clickNumberButton('5');
-    // }
-
-    // if (e.target.parentElement.id === 'number-button-6') {
-    //   clickNumberButton('6');
-    // }
-
-    // if (e.target.parentElement.id === 'number-button-7') {
-    //   clickNumberButton('7');
-    // }
-
-    // if (e.target.parentElement.id === 'number-button-8') {
-    //   clickNumberButton('8');
-    // }
-
-    // if (e.target.parentElement.id === 'number-button-9') {
-    //   clickNumberButton('9');
-    // }
+    if (e.target.parentElement.id === 'number-button-5') {
+      clickNumberButton('5');
+    }
 
     if (e.target.parentElement.id === 'add-button') {
       clickOperationButton(add);
@@ -152,14 +114,6 @@ function setupEventListeners() {
 
     if (e.target.parentElement.id === 'subtract-button') {
       clickOperationButton(subtract);
-    }
-
-    if (e.target.parentElement.id === 'multiply-button') {
-      clickOperationButton(multiply);
-    }
-
-    if (e.target.parentElement.id === 'divide-button') {
-      clickOperationButton(divide);
     }
 
     if (e.target.parentElement.id === 'equals-button') {
